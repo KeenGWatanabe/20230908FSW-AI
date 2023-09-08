ebf4bba366c746dde6987e8e51c1f24faf1e6d72
KeenGWatanabe <mckeenweb@gmail.com>
2023-09-08 15:19:23 +0800

    update

--- script.js
+++ script.js
@@ -11,10 +11,7 @@ function myFunction() {
 	else if(inputText.toLowerCase().indexOf("bullshit")>-1){
 		document.getElementById("answer").innerHTML="as long as people exist, bullshit everywhere";
 		}
-	else if(inputText.toLowerCase().indexOf("i ")>-1){
-		document.getElementById("answer").innerHTML=`Congratulations! WE ALL GONNA DIE! <br> You hit the sweetspot of humanity, the word "I" `;
-		}
-		else if(inputText.toLowerCase().indexOf("god")>-1){
+	else if(inputText.toLowerCase().indexOf("god")>-1){
 		document.getElementById("answer").innerHTML='<a href="https://www.theguardian.com/education/2004/mar/08/highereducation.uk1#:~:text=A%20scientist%20has%20calculated%20thatexistence%20of%20an%20omnipotent%20being."  target="_blank">A scientist has calculated that there is a 67% chance that God exists</a>'; 
 		}
 	else if(inputText.toLowerCase().indexOf("the developer")>-1){
